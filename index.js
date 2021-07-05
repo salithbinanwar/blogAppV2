1b8c2e2833e9499edb3ab9528dff13367e698947
salithbinanwar <salithanwar69@gmail.com>
2021-07-06 01:38:10 +0600

    path added

--- index.js
+++ index.js
@@ -11,22 +11,26 @@ const blogData = require('./blogData/blogData')
 // app initialization
 app.use(cors());
 app.use(express.json())
-
+app.use(express.static('build'))
 // connecting with mongoose
 
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true, useUnifiedTopology: true
-}).then(()=> {
+}).then(() => {
     console.log('db connected');
 }).catch(err => console.log(err))
 
 // show data
-app.use('/all', blogData );
+app.use('/all', blogData);
 
-app.get('/',(req , res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './build/index.html'))
 })
 
-app.listen(port, ()=>{
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(port, () => {
     console.log(`running on port ${port}`);
 });
